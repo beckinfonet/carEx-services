ef4ad75b4d5792877fd64aa1967ed0bd9bbdd92b
beckinfonet <bakytbek.tatibekov@gmail.com>
2026-01-30 00:53:31 -0800

    db name update

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI, { dbName: 'CarEx' })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
